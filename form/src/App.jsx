e1a4f4b7b4eb5680b933f5d7450109f5b42e5386
Kaique Fiochi <k.fiocchi2009@gmail.com>
2025-02-04 14:16:14 -0300

    Setup

---
+++ form/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  
+return(
+  <div className='App'>
+<h1>Formulário Multistep</h1>
+
+
+ </div>
+)
+}
+
+export default App
