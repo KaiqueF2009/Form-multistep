e7db7560b26a3ebd90c51ed1ae328ad6e6bc9ab1
Kaique Fiochi <k.fiocchi2009@gmail.com>
2025-02-12 14:06:35 -0300

    Thanks html

--- form/src/components/Thanks.jsx
+++ form/src/components/Thanks.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import {BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsEmojiNeutralFill, BsEmojiAngryFill} from 'react-icons/bs'
+import './Thanks.css'
 const Thanks = () => {
   return (
-    <div>
-      <h2>Thanks</h2>
+    <div className='thanks-container'>
+      <h2>Falta só um pouco...</h2>
+      <p>Sua opinião é importante para nós, em breve receberá um cupom de 10% de desconto para a próxima compra.</p>
+      <p>Para concluir a avaliação, clique no botão de envio</p>
+      <h3>Essa é sua avaliação:</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+      </p>
     </div>
   )
 }
